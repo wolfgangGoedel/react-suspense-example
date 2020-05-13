302946a80c961b73a78bad920ff4a122a1211614
Wolfgang Goedel <wgoedel@gmail.com>
2020-05-13 15:19:03 +0200

    image !

--- src/App.tsx
+++ src/App.tsx
@@ -2,36 +2,19 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import ErrorBoundary from "./components/ErrorBoundary";
-import WaitForMe from "./components/WaitForMe";
-import wrapPromise from "./utils/wrapPromise";
-
-const defer = (msg: string) =>
-  new Promise<string>((resolve, reject) => {
-    setTimeout(() => {
-      resolve(msg);
-      // reject(new Error("Rejected"));
-    }, 2000);
-  });
+import Img from "./components/Img";
 
 const App = () => {
-  const [resource, setResource] = React.useState(wrapPromise(defer("Yay")));
-
   return (
     <div className="App">
       <header className="App-header">
         <ErrorBoundary fallback={<div>Oops</div>}>
           <React.Suspense fallback="Loading …">
             <img src={logo} className="App-logo" alt="logo" />
-            <WaitForMe resource={resource} />
-            <button
-              onClick={() => {
-                setResource(
-                  wrapPromise(defer("Yay at " + new Date().toLocaleString()))
-                );
-              }}
-            >
-              New Resource
-            </button>
+            <Img
+              src="https://www.nikgraf.com/static/portrait.jpg"
+              alt="This is Nik"
+            />
           </React.Suspense>
         </ErrorBoundary>
       </header>
