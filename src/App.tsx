fe1f2e2585bd9880d24c2f3c80cef28a475bba67
Wolfgang Goedel <wgoedel@gmail.com>
2020-05-13 15:13:00 +0200

    L4

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import WaitForMeTwoSec from './components/WaitForMeTwoSec';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import ErrorBoundary from "./components/ErrorBoundary";
+import WaitForMe from "./components/WaitForMe";
+import wrapPromise from "./utils/wrapPromise";
 
-function App() {
+const defer = (msg: string) =>
+  new Promise<string>((resolve, reject) => {
+    setTimeout(() => {
+      resolve(msg);
+      // reject(new Error("Rejected"));
+    }, 2000);
+  });
+
+const App = () => {
+  const [resource, setResource] = React.useState(wrapPromise(defer("Yay")));
+
   return (
     <div className="App">
       <header className="App-header">
-        <React.Suspense fallback="loading...">
-          <img src={logo} className="App-logo" alt="logo" />
-          <WaitForMeTwoSec />
-        </React.Suspense>
+        <ErrorBoundary fallback={<div>Oops</div>}>
+          <React.Suspense fallback="Loading …">
+            <img src={logo} className="App-logo" alt="logo" />
+            <WaitForMe resource={resource} />
+            <button
+              onClick={() => {
+                setResource(
+                  wrapPromise(defer("Yay at " + new Date().toLocaleString()))
+                );
+              }}
+            >
+              New Resource
+            </button>
+          </React.Suspense>
+        </ErrorBoundary>
       </header>
     </div>
   );
-}
+};
 
 export default App;
